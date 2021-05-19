99a10f53279acde72aecc2f44b1f57e09daa5a7b
pipe2442 <felipeangulo2442@gmail.com>
2021-05-19 11:33:30 -0500

    Create library functions and display methods

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var library = [];
 
-function book(title, author, date) {
+var title_input = document.getElementById("title");
+var author_input = document.getElementById("author");
+var date_input = document.getElementById("date");
+var read_input = document.getElementById("read");
+
+
+
+function Book(title, author, date, read) {
     this.title = title;
     this.author = author;
     this.date = date
+    this.read = read
 };
 
+
 function add_book() {
-    
+    var newBook = new Book(title_input, author_input, date_input, read_input)
+    library.push(newBook.title.value+newBook.author.value+newBook.date.value)
+    show_library();
 };
+
+function show_library() {
+    document.getElementById("display").innerHTML = library;
+};
+
+
