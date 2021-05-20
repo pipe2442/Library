1e893420bcaa3d968d9d6b764d2d93e235297ec8
pipe2442 <felipeangulo2442@gmail.com>
2021-05-20 17:39:59 -0500

    Fix linter errors

--- app.js
+++ app.js
@@ -2,58 +2,64 @@ const library = [];
 
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
 const dateInput = document.getElementById('date');
 const readInput = document.getElementById('read');
 
-function Book(title, author, date, read) {
+function Book(title, author, pages, date, read) {
   this.title = title;
   this.author = author;
+  this.pages = pages;
   this.date = date;
   this.read = read;
 }
 
-function add_book() {
-  const newBook = new Book(titleInput, authorInput, dateInput, readInput);
+function showLibrary() {
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  dateInput.value = '';
+
+  document.getElementById('display').innerHTML = '';
+  for (let i = 0; i < library.length; i += 1) {
+    document.getElementById('display').innerHTML
+          += `<div class="d-flex justify-content-center flex-column text-center m-5 card bg-light p-5 rounded shadow">
+              <div>
+                  Title: ${`${library[i].title}<br> Author: ${library[i].author}<br> Pages: ${library[i].pages}<br> Publish Date: ${library[i].date}<br> Read Status: ${library[i].read}`}
+              </div>
+              <div>
+                  <button class="btn btn-primary my-2" onclick="deleteBook(library, ${i});">Delete</button><br>
+                  <button class="btn btn-primary my-2" onclick="readStatus(library, ${i});">Read Status</button><br>
+              </div>
+          </div>`;
+  }
+}
+
+function addBook() {
+  const newBook = new Book(titleInput, authorInput, pagesInput, dateInput, readInput);
   library.push({
     title: newBook.title.value,
     author: newBook.author.value,
+    pages: newBook.pages.value,
     date: newBook.date.value,
     read: newBook.read.value,
   });
 
-  show_library();
+  showLibrary();
 }
 
-function delete_book(arr, index) {
+function deleteBook(arr, index) {
   arr.splice(index, 1);
-  show_library();
+  showLibrary();
 }
 
-function read_status(arr, i) {
-  if (arr[i].read == 'yes') {
+function readStatus(arr, i) {
+  if (arr[i].read === 'yes') {
     arr[i].read = 'no';
   } else {
     arr[i].read = 'yes';
   }
-  show_library();
+  showLibrary();
 }
 
-function show_library() {
-  titleInput.value = '';
-  authorInput.value = '';
-  dateInput.value = '';
-
-  document.getElementById('display').innerHTML = '';
-  for (i = 0; i < library.length; i++) {
-    document.getElementById('display').innerHTML
-        += `<div class="d-flex justify-content-center flex-column text-center m-5 card bg-light p-5 rounded shadow">
-            <div>
-                Title: ${`${library[i].title}<br> Author: ${library[i].author}<br> Publish Date: ${library[i].date}<br> Read Status: ${library[i].read}`}
-            </div>
-            <div>
-                <button class="btn btn-primary my-2" onclick="delete_book(library, ${i});">Delete</button><br>
-                <button class="btn btn-primary my-2" onclick="read_status(library, ${i});">Read Status</button><br>
-            </div>
-        </div>`;
-  }
-}
+console.log(addBook, deleteBook, readStatus);
