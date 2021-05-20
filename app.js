72d674061691e8f2e29f0dce8a870d181c63697a
pipe2442 <felipeangulo2442@gmail.com>
2021-05-20 12:16:35 -0500

    Add delete button to library

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var author_input = document.getElementById("author");
 var date_input = document.getElementById("date");
 var read_input = document.getElementById("read");
 
-
-
 function Book(title, author, date, read) {
     this.title = title;
     this.author = author;
@@ -14,15 +12,22 @@ function Book(title, author, date, read) {
     this.read = read
 };
 
-
 function add_book() {
     var newBook = new Book(title_input, author_input, date_input, read_input)
-    library.push(newBook.title.value+newBook.author.value+newBook.date.value)
-    show_library();
+    library.push(`Title: ${newBook.title.value} <br>`+`Author: ${newBook.author.value} <br>`+
+    `Realease date: ${newBook.date.value} <br>`+`Readed: ${newBook.read.value} <br>` )
+    show_library(); 
 };
+
+function delete_book(arr, index){
+    arr.splice(index, 1);
+    show_library(); 
+}
 
 function show_library() {
-    document.getElementById("display").innerHTML = library;
+    document.getElementById("display").innerHTML = "";
+    for(i = 0; i<library.length; i++){
+        document.getElementById("display").innerHTML += library[i]+
+        `<button onclick="delete_book(library, ${i});">Delete</button><br>`;
+    }
 };
-
-
