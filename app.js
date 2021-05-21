0334398b11d3193454778e69554b21905dc611d8
pipe2442 <felipeangulo2442@gmail.com>
2021-05-21 11:37:49 -0500

    Create modal to hide the form and show it with add new book button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars, func-names */
 const library = [];
 
 const titleInput = document.getElementById('title');
@@ -62,3 +62,23 @@ function readStatus(arr, i) {
   }
   showLibrary();
 }
+
+const modal = document.getElementById('myModal');
+
+const btn = document.getElementById('myBtn');
+
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
